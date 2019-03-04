11c387c089c837247a5a0d04579febb86d47d133
Michiel van Rooy <michielvanrooy@gmail.com>
2019-03-04 19:21:37 +0200

    MVC project added

---
+++ RaspberryRobot/IRaspberryRobot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RaspberryRobot
+{
+    public interface IRaspberryRobot : IDisposable
+    {
+        void RedLED(bool value);
+        //void YellowLED(bool value);
+        //void GreenLED(bool value);
+        //void BlueLED(bool value);
+    }
+}
